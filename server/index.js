9162741bb483852e896ea8b6468eb494af39b280
JoshGenereux <josh.genereux13@gmail.com>
2021-12-16 13:47:09 -0700

    moved stuff around in index.js

--- server/index.js
+++ server/index.js
@@ -3,13 +3,13 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const path = require('path')
-app.use(express.json())
-app.use(cors())
 const ctrl = require('./controller')
 const {SERVER_PORT: SP} = process.env
+app.use(express.json())
+app.use(cors())
 
 app.get('/', (req,res)=>{
-    res.sendfile(path.join(__dirname, 'front/welcome-page.html'))
+    res.sendfile(path.join(__dirname, '/front/welcome-page.html'))
 })
 
 app.post('/password-manager/sign-in', ctrl.signIn);
